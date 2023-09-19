c848f4008f9165159af603d2fd8578a05eb947af
Esteban <cepalacios@outlook.com>
2023-09-19 13:19:43 -0500

    Implement calculator layout using grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="app">
-      <header className="app-header">
-        <img src={logo} className="app-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="app-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="app-main">
+        <Calculator />
+      </main>
     </div>
   );
 }
